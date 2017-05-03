b09734183728e9862c3f997bbcf90330024fb1b6
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-05-03 11:20:19 -0700

    setup dotenv and connected to mongodb

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require("dotenv").config();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,11 +17,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// for Angular during development
+app.use(cors());
+
+// connect to the database
+mongoose.connect(process.env.MONGODB_URI);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
